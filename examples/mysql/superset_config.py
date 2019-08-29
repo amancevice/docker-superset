5f91076b340040a490f2ca56ab15cf03266e1bca
Alexander Mancevice <amancevice@cargometrics.com>
2019-08-29 08:50:08 -0400

    example MySQL uses UTF-8

--- examples/mysql/superset_config.py
+++ examples/mysql/superset_config.py
@@ -9,6 +9,7 @@ CACHE_CONFIG = {
     'CACHE_REDIS_PORT': 6379,
     'CACHE_REDIS_DB': 1,
     'CACHE_REDIS_URL': 'redis://redis:6379/1'}
-SQLALCHEMY_DATABASE_URI = 'mysql://superset:superset@mysql:3306/superset'
+SQLALCHEMY_DATABASE_URI = \
+    'mysql://superset:superset@mysql:3306/superset?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = 'thisISaSECRET_1234'
