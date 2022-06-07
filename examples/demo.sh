1e6629e263d9229f4a6940b66644f0a1e848eba2
Alexander Mancevice <alexander.mancevice@hey.com>
2022-06-07 08:29:45 -0400

    update demo script to use docker<space>compose

--- examples/demo.sh
+++ examples/demo.sh
@@ -15,34 +15,34 @@ cd $1
 # Start back end
 if [ "$1" == "sqlite" ]; then
   echo "Starting redis service..."
-  docker-compose up -d redis
+  docker compose up -d redis
   >| ./superset/superset.db
 elif [ "$1" == "mysql" ] || [ "$1" == "postgres" ]; then
   echo "Starting redis & $1 services..."
-  docker-compose up -d redis $1
+  docker compose up -d redis $1
   echo "Sleeping for 30s"
   sleep 30
 else
   echo "Starting redis & postgres services..."
-  docker-compose up -d redis postgres
+  docker compose up -d redis postgres
   echo "Sleeping for 30s"
   sleep 30
 fi
 
 # Start Superset
 echo "Starting Superset..."
-docker-compose up -d superset
+docker compose up -d superset
 if [ "$1" == "celery" ]; then
   echo "Starting Superset worker..."
-  docker-compose up -d worker
+  docker compose up -d worker
 fi
 echo "Sleeping for 30s"
 sleep 30
 
 # Inititalize Demo
-docker-compose exec superset superset-demo
+docker compose exec superset superset-demo
 
 echo "Navigate to http://localhost:8088 to view demo"
 echo -n "Press RETURN to bring down demo"
 read down
-docker-compose down -v
+docker compose down -v
